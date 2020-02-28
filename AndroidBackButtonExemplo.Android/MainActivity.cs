551558627fcaa7be9604c7c872647bc329fa4a44
Thiago Bertuzzi <thiago.bertuzzi@gmail.com>
2020-02-28 08:47:02 -0300

    :tada: Subindo exemplo para o Github

---
+++ AndroidBackButtonExemplo.Android/MainActivity.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace AndroidBackButtonExemplo.Droid
+{
+    [Activity(Label = "AndroidBackButtonExemplo", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(savedInstanceState);
+
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            LoadApplication(new App());
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
+        //Confirmação se o App vai Fechar via Modal
+        public override void OnBackPressed()
+        {
+            if (((AndroidBackButtonExemplo.App)App.Current).ConfirmaFecharApp)
+            {
+                using (var alert = new AlertDialog.Builder(this))
+                {
+                    alert.SetTitle("Vou fechar em");
+                    alert.SetMessage("Tem certeza que deseja sair do APP?");
+                    alert.SetPositiveButton("Sim", (sender, args) => { FinishAffinity(); });
+                    alert.SetNegativeButton("Não", (sender, args) => { });
+
+                    var dialog = alert.Create();
+                    dialog.Show();
+                }
+                return;
+            }
+            base.OnBackPressed();
+        }
+
+        //Confirmação se o App vai Fechar via Toast
+        //private bool _isBackPressed;
+        //public override void OnBackPressed()
+        //{
+        //    if (((AndroidBackButtonExemplo.App)App.Current).ConfirmaFecharApp)
+        //    {
+        //        using (var alert = new AlertDialog.Builder(this))
+        //        {
+        //            Android.Widget.Toast.MakeText(this, "Pressione mais uma vez para fechar", Android.Widget.ToastLength.Short).Show();
+
+        //            // O Alerta vai sumir em 2 segundos
+        //            new Handler().PostDelayed(() =>
+        //            {
+        //                _isBackPressed = false;
+        //            }, 2000);
+        //        }
+        //        return;
+        //    }
+        //    base.OnBackPressed();
+        //}
+    }
+}
---
+++ AndroidBackButtonExemplo/App.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AndroidBackButtonExemplo
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+
+        public bool ConfirmaFecharApp
+        {
+            get
+            {
+                bool ultimaView = false;
+                switch (MainPage)
+                {
+                    // Unica Pagina
+                    case ContentPage _:
+                        ultimaView = true;
+                        break;
+                    // É NavigationPage ou TabbedPage
+                    case NavigationPage mainPage when mainPage.CurrentPage is TabbedPage tabbedPage
+                                                      && tabbedPage.CurrentPage is NavigationPage navigationPage:
+                        ultimaView = navigationPage.Navigation.NavigationStack.Count <= 1;
+                        break;
+                    // A NavigationPage é a Pagina Principal
+                    case NavigationPage mainPage:
+                        ultimaView = mainPage.Navigation.NavigationStack.Count <= 1;
+                        break;
+                    // A TabbedPage é a Pagina Principal
+                    case TabbedPage tabbedPage when tabbedPage.CurrentPage is NavigationPage navigationPage:
+                        ultimaView = navigationPage.Navigation.NavigationStack.Count <= 1;
+                        break;
+                }
+                return ultimaView;
+            }
+        }
+    }
+}
